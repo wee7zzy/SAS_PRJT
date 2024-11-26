ccc4c0eb0a7e7dd4b4541bcc37f397742941289e
wee7zzy <yassaniq@gmail.com>
2024-11-26 15:07:54 +0100

    ici j'ai cree la fonction ajouter pour que l'utilisateur ajoute les taches qui voudrais executer .

--- PROJETSAS.C
+++ PROJETSAS.C
@@ -13,6 +13,24 @@ typedef struct {
     Priorite priorite;
 } Tache;
 
+Tache taches[100];
+int nmbr_taches=0,i;
+
+void ajouter(int prio) {
+    printf("Ajout d'une nouvelle tâche :\n");
+    printf("Titre : ");
+    getchar();
+    fgets(taches[nmbr_taches].titre, sizeof(taches[nmbr_taches].titre),stdin);
+    printf("Description : ");
+    fgets(taches[nmbr_taches].description, sizeof(taches[nmbr_taches].description),stdin);
+    printf("Date d'échéance (JJ/MM/AAAA) : ");
+    fgets(taches[nmbr_taches].date_echeance, sizeof(taches[nmbr_taches].date_echeance),stdin);
+    printf("Priorité (1 = Haute, 0 = Basse) : ");
+    scanf("%d",&prio);
+    taches[nmbr_taches].priorite = (prio == 1) ? HAUTE : BASSE;
+    nmbr_taches++;
+}
+
 int main() {
 
 return 0;
